ac89690136e4adf8aa4c6e5ceb05d6cf8431b36f
deathis <p.puchalski@yahoo.pl>
2013-04-15 16:33:53 +0200

    Order and OrderItems in OrderController

--- EasyERP/Areas/Admin/Controllers/OrderController.cs
+++ EasyERP/Areas/Admin/Controllers/OrderController.cs
@@ -35,69 +35,6 @@ namespace EasyERP.Areas.Admin.Controllers
             return View(new OrderDetails(order, orderItems.ToList()));
         }
 
-        public ActionResult Create()
-        {
-            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name");
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Orders.Add(order);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        public ActionResult Edit(int id = 0)
-        {
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(order).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        public ActionResult Delete(int id = 0)
-        {
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Order order = db.Orders.Find(id);
-            db.Orders.Remove(order);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
