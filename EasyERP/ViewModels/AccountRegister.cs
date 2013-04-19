cc4c3629d6e1a3d0835bc60db65a7c35f80b5b00
deathis <p.puchalski@yahoo.pl>
2013-04-19 10:39:07 +0200

    new AccountRegister in ViewModels

---
+++ EasyERP/ViewModels/AccountRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EasyERP.Models;
+
+namespace EasyERP.ViewModels
+{
+    public class AccountRegister
+    {
+        public RegisterModel RegisterModel { get; set; }
+        public Customer Customer { get; set; }
+
+        public AccountRegister(RegisterModel RegisterModel, Customer Customer)
+        {
+            this.RegisterModel = RegisterModel;
+            this.Customer = Customer;
+        }
+    }
+}
