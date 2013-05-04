33dbfe81d32489c962719732138cb64f8bd74a77
deathis <p.puchalski@yahoo.pl>
2013-05-05 00:43:09 +0200

    Configurator model which contains list of materials in data session

---
+++ EasyERP/Models/Configurator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyERP.Models
+{
+    public class Configurator
+    {
+        private HttpContextBase httpContext { get; set; }
+        private const string ConfiguratorSessionKey = "ConfiguratorId";
+
+        public static Configurator GetInstance(HttpContextBase databaseContext)
+        {
+            Configurator configuratorContext = new Configurator();
+            configuratorContext.httpContext = databaseContext;
+
+            if (databaseContext.Session[ConfiguratorSessionKey] == null)
+            {
+                databaseContext.Session[ConfiguratorSessionKey] = new List<Material>();
+            }
+
+            return configuratorContext;
+        }
+
+        public List<Material> GetConfiguration()
+        {
+            return (List<Material>)httpContext.Session[ConfiguratorSessionKey];
+        }
+
+        public void AddToConfiguration(Material material)
+        {
+            List<Material> materials = GetConfiguration();
+
+            if (materials.Find(m => m.Id == material.Id) == null)
+            {
+                materials.Add(material);
+            }
+
+            httpContext.Session[ConfiguratorSessionKey] = materials;
+        }
+
+        public void RemoveFromConfiguration(Material material)
+        {
+            List<Material> materials = GetConfiguration();
+            materials.Remove(material);
+
+            httpContext.Session[ConfiguratorSessionKey] = materials;
+        }
+    }
+}
