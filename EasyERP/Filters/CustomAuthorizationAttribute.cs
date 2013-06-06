ebe4a2adb6d155719fb0af675ca6649f11949dbd
defis <deathis@o2.pl>
2013-06-06 17:38:55 +0200

    aythorization fixed

--- EasyERP/Filters/CustomAuthorizationAttribute.cs
+++ EasyERP/Filters/CustomAuthorizationAttribute.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using WebMatrix.WebData;
+using System.Web.Mvc;
 
 namespace EasyERP.Filters
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited=true, AllowMultiple=true)]
     public class CustomAuthorizationAttribute : AuthorizeAttribute
     {
-        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             if (!WebSecurity.Initialized)
             {
                 WebSecurity.InitializeDatabaseConnection("DatabaseContext", "UserProfile", "UserId", "UserName", autoCreateTables: false);
             }
- 	        
-            base.OnAuthorization(actionContext);
+            return base.AuthorizeCore(httpContext);
         }
     }
 }
